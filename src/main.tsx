9874322954b8988b36a863cfd87289ba44464dfd
Illia Ilin <ilin.i.1337@gmail.com>
2024-08-28 19:52:52 +0200

    initialization/INIT-Initialize-application
    - Create basic app class component
    - Remove unused styles
    - Update vite config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+// outsource dependencies
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// local dependencies
 import App from './App.tsx';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
