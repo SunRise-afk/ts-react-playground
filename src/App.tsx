8eb8bb02ffc181e99adb3b7e7c7177ba39ffb5ae
Illia Ilin <ilin.i.1337@gmail.com>
2024-08-28 20:05:57 +0200

    feature/APP-Type-class-component
    - Type class component
    - Add class component props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 // outsource dependencies
 import React from 'react';
 
-class App extends React.PureComponent {
+// configure
+interface AppProps {
+  color?: string;
+}
+
+interface AppState {
+  counter: number;
+}
+
+class App extends React.PureComponent<AppProps, AppState> {
+  constructor (props:AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
   render () {
-    return <div>Hi There</div>;
+    return <div>
+      <button onClick={this.onIncrement}>Increment</button>
+      <button onClick={this.onDecrement}>Decrement</button>
+      <div>{ this.state.counter }</div>
+    </div>;
   }
 }
 
